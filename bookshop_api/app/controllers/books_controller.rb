44f7739e410825af876ee1ee7caf99280895e3b4
Charlie Wood <charliemowood@gmail.com>
2017-07-22 00:03:03 +0100

    Add show route to books

--- bookshop_api/app/controllers/books_controller.rb
+++ bookshop_api/app/controllers/books_controller.rb
@@ -3,4 +3,8 @@ class BooksController < ApplicationController
     @books = Book.all
     render json: @books.as_json({except: [:created_at, :updated_at]})
   end
+
+  def show
+		render json: Book.find(params[:id])
+	end
 end
