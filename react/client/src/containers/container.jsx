d6dba6263868511e016b4807663486e3e2d17d02
Charlie Wood <charliemowood@gmail.com>
2017-07-25 17:22:48 +0100

    Add 1 title to display on the screen

--- react/client/src/containers/container.jsx
+++ react/client/src/containers/container.jsx
@@ -2,11 +2,45 @@ import React from 'react'
 import { Dropdown } from '../components/dropdown.jsx'
 
 class Container extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      bookNames: [{title:null}],
+      // focusName: null
+    };
+    // State: countries, currentCountry
+  }
+
+
+    componentDidMount() {
+      const url = "http://localhost:5000/api/books";
+      const request = new XMLHttpRequest();
+      request.open("GET", url);
+
+      request.onload = () => {
+        if (request.status !== 200) return
+
+        const jsonString = request.responseText;
+        const data = JSON.parse(jsonString);
+
+        this.setState({ bookNames: data})
+      }
+
+      request.send();
+    }
+
+
 
   render() {
 
+// why does this render twice?
+console.log(this.state.bookNames[0])
     return(
-      <Dropdown/>
+      // why can I not console log in here?
+      // <Dropdown/>
+      <div>
+     <h1>{this.state.bookNames[0].title}</h1>
+     </div>
     )
   }
 
