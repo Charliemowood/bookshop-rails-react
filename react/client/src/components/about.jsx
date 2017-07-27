4056a2834f1f93a5fc7209e5c8c5ca0306a004ac
Charlie Wood <charliemowood@gmail.com>
2017-07-27 08:35:09 +0100

    Add images

--- react/client/src/components/about.jsx
+++ react/client/src/components/about.jsx
@@ -3,6 +3,9 @@ import React from 'react'
 const About = () => (
   <div>
     <p>This book management was crafted using rails and react.</p>
+    <img src="./images/react.png" className="spaced"/>
+    <img src="./images/rails.png" className="spaced"/>
+
   </div>
 )
 
--- react/client/src/components/home.jsx
+++ react/client/src/components/home.jsx
@@ -3,6 +3,7 @@ import React from 'react'
 const Home = () => (
   <div>
     <p>Welcome to our magical homepage, this is where the magical book management happens.</p>
+    <img src='./images/Czech.png'/>
   </div>
 )
 
